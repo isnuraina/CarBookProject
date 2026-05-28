d90c57f3d3b215c6458aa42142aa80a5db736f42
isnuraina <nurane.ismayilzade19@gmail.com>
2026-05-28 22:51:25 +0400

    47 is added

--- Infrastructure/CarBookProject.Persistence/Context/CarBookContext.cs
+++ Infrastructure/CarBookProject.Persistence/Context/CarBookContext.cs
@@ -12,7 +12,7 @@ namespace CarBookProject.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=RB00-IT028\\MSSQLSERVER04;initial Catalog=CarBookDb;integrated Security=true ;TrustServerCertificate=true;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-MPV3150\\SQLEXPRESS;initial Catalog=CarBookDb;integrated Security=true ;TrustServerCertificate=true;");
         }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Banner> Banners { get; set; }
---
+++ Infrastructure/CarBookProject.Persistence/Migrations/20260523181006_miq3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBookProject.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class miq3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
